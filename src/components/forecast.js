ebfb211f7619ff605183f7b86a2e8437805c5ac4
mhfong <mhfong@connect.ust.hk>
2022-04-08 19:52:30 +0800

    update forecast.js

--- src/components/forecast.js
+++ src/components/forecast.js
@@ -11,6 +11,14 @@ const models = [
     { value: '2', label: 'GRU' }
 ]
 
+const days = [
+    { value: '1', label: '1' },
+    { value: '2', label: '2' },
+    { value: '3', label: '3' },
+    { value: '4', label: '4' },
+    { value: '5', label: '5' }
+]
+
 class Forecast extends Component {
     state = {  } 
     render() { 
@@ -34,10 +42,24 @@ class Forecast extends Component {
                                     <Select defaultValue={models[0]} options={models} isDisabled={true}/>
                                 </div>
                         </div>
+                        <h2>+</h2>
+                        <div className="input">
+                                <div className='model-select'>
+                                    <span className="material-icons-sharp">date_range</span>
+                                    <h2>Predict days</h2>
+                                    <Select defaultValue={days[4]} options={days} isDisabled={true}/>
+                                </div>
+                        </div>
+                        <h2>-{'>'}</h2>
+                        <div className="submit">
+                                <button className='btn2'>
+                                    <h2>START</h2>
+                                </button>
+                        </div>
                     </div>
                     <h4 className='desc'>
                         <span className="material-icons-sharp">info</span>
-                        We will suggest the best combination of our AI solution to predict the price for you. </h4>
+                        We suggest the best combination of our AI solution to predict the price for you. </h4>
                     <div className="predict">
                         <div></div>
                         <div></div>
